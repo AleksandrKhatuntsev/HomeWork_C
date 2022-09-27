5f3fc9af7482eee13880056ca3ae3647952c4353
Aleksandr Khatuntsev <hatuntsev.alexander@yandex.ru>
2022-09-27 20:02:04 +0300

    Семинар1

---
+++ Seminar_1/Home_work_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count <= num)
+{
+    Console.Write(count + ", ");
+    count = count + 2;
+}
