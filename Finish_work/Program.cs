0e2db7899d8b94d3982f9f6ae9c861c849715016
Aleksandr Khatuntsev <hatuntsev.alexander@yandex.ru>
2022-11-21 20:51:44 +0300

    Исправление цикла

---
+++ Finish_work/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] word1 = new string[5] {"123", "aa", "hello", "world", "qwerty"};
+string[] word2 = new string[word1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(word1, word2);
+PrintArray(word2);
